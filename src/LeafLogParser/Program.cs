85d890a576a9ee0c8224ad6d811062a0018fa58a
Nicholas J Dobbins <ndobb@uw.edu>
2019-07-26 14:20:44 -0700

    Updated

---
+++ src/LeafLogParser/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using CommandLine;
+using Model;
+using System.Collections.Generic;
+
+namespace LeafLogParser
+{
+    class Program
+    {
+        static AppSettings settings = new AppSettings();
+
+        static void Main(string[] args)
+        {
+            LoadSettings(args);
+
+            /* VS debug
+            settings.SourceDirPath = "C:/NicWork/apps/logs/leaf";
+            settings.OutputDirPath = settings.SourceDirPath + "/archive";
+            settings.DbConnection = "Server=localhost;Database=LeafLog;Integrated Security=SSPI";
+            settings.DbTable = "dbo.UsageLog";
+            settings.BatchSize = 1000;
+            settings.IgnoreTypes = new string[] { "Refreshed TokenBlacklistCache" };
+            settings.IgnoreToday = true;
+            settings.MoveCompleted = true;
+            */
+
+            CheckDirectories();
+            CheckDatabase();
+
+            var manager = new LogEntryTransferManager(settings);
+            var reader = new LogReader(settings, manager);
+
+            while (reader.Read())
+            {
+                reader.Process();
+            }
+
+            Console.WriteLine($"Successfully copied {manager.CopyCount} log entries from {reader.FileCount} files.");
+            Console.WriteLine($"Exiting...");
+        }
+
+        static void LoadSettings(string[] args)
+        {
+            Parser.Default.ParseArguments<AppSettings>(args)
+                .WithParsed(s => settings = s);
+
+            // If no separator in the output path, assume it's within the source directory
+            if (!settings.OutputDirPath.Contains(Path.DirectorySeparatorChar))
+            {
+                var outname = settings.OutputDirPath;
+                settings.OutputDirPath = $"{settings.SourceDirPath}{Path.DirectorySeparatorChar}{outname}";
+            }
+        }
+
+        static void CheckDirectories()
+        {
+            var message = "";
+
+            if (!Directory.Exists(settings.SourceDirPath))
+            {
+                message = $"The source directory '{settings.SourceDirPath}' could not be found.";
+                Console.WriteLine(message);
+                throw new DirectoryNotFoundException(message);
+            }
+
+            if (!Directory.Exists(settings.OutputDirPath))
+            {
+                message = $"The output directory '{settings.OutputDirPath}' could not be found. Attempting to create...";
+                Console.WriteLine(message);
+
+                try
+                {
+                    Directory.CreateDirectory(settings.OutputDirPath);
+                    message = $"Output directory '{settings.OutputDirPath}' created.";
+                    Console.WriteLine(message);
+                }
+                catch (Exception ex)
+                {
+                    message = $"The output directory '{settings.OutputDirPath}' could not be created: {ex.Message}";
+                    throw new Exception(message, ex.InnerException);
+                }
+            }
+
+            message = $"Directories '{settings.SourceDirPath}' and '{settings.OutputDirPath}' successfully validated.";
+            Console.WriteLine(message);
+        }
+
+        static void CheckDatabase()
+        {
+            var message = "";
+            var sql = $"SELECT TOP 1 * FROM {settings.DbTable}";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(settings.DbConnection))
+                {
+
+                    conn.Open();
+                    var cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+
+                    message = $"SQL Server '{conn.Database}', table '{settings.DbTable}' successfully connected.";
+                    Console.WriteLine(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                message = $"The SQL Server returned an error: {ex.Message}";
+                throw new ArgumentException(message, "t", ex.InnerException);
+            }
+        }
+    }
+}
---
+++ src/Model/AppSettings.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace Model
+{
+    public class AppSettings
+    {
+        [Option(
+            's',
+            "source",
+            Required = true,
+            HelpText = "The target directory to extract Leaf logs from."
+        )]
+        public string SourceDirPath { get; set; }
+
+        [Option(
+            'o',
+            "output",
+            Required = false,
+            Default = "archive",
+            HelpText = "The output directory to move Leaf logs to after processing."
+        )]
+        public string OutputDirPath { get; set; }
+
+        [Option(
+            'd',
+            "database",
+            Required = true,
+            HelpText = "The database connection string to connect to SQL Server. " +
+                       "Should be of the form 'Server=<address>;Database=<db_name>;User Id=<user_name>;'Password=<pass>;Integrated Security=<optional, if using Windows auth then 'SSPI', omit pass and user>.")]
+        public string DbConnection { get; set; }
+
+        [Option(
+            't',
+            "table",
+            Required = true,
+            HelpText = "The database table in which to copy the log file information into. Should be of the form <schema>.<table_name>."
+        )]
+        public string DbTable { get; set; }
+
+        [Option(
+            'b',
+            "batch-size",
+            Required = false,
+            Default = 1000,
+            HelpText = "Number of log records to build insert into the database at a time."
+        )]
+        public int BatchSize { get; set; }
+
+        [Option(
+            'i',
+            "ignore--message-types",
+            Required = false,
+            Separator = ',',
+            Default = new string[] { "Refreshed TokenBlacklistCache" },
+            HelpText = "Log entry MessageTemplate types to ignore, delimited by ','."
+        )]
+        public IEnumerable<string> IgnoreTypes { get; set; }
+
+        [Option(
+            'c',
+            "ignore-today",
+            Required = false,
+            Default = true,
+            HelpText = "Boolean indicating whether files named with the same date as the current date should be ignored."
+        )]
+        public bool IgnoreToday { get; set; }
+
+        [Option(
+            'm',
+            "move-to-archive",
+            Required = false,
+            Default = true,
+            HelpText = "Boolean indicating whether completed log files should be moved to the output."
+        )]
+        public bool MoveCompleted { get; set; }
+    }
+}
